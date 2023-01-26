fc1e60be7000a7a0c7fdecc2717fa41a4192108d
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-01-26 13:49:48 +0700

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -2,7 +2,6 @@ from data import Data
 from pyrogram.types import Message
 from telethon import TelegramClient
 from pyrogram import Client, filters
-from pyrogram1 import Client as Client1
 from asyncio.exceptions import TimeoutError
 from telethon.sessions import StringSession
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
